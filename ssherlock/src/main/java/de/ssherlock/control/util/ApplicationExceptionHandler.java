40b0264ba14375874ce6f1d453cb48e0141ddb8d
Victor Vollmann <vollmannv@gmail.com>
2023-11-07 16:41:11 +0100

    added exception handler

---
+++ ssherlock/src/main/java/de/ssherlock/control/util/ApplicationExceptionHandler.java
@@ -0,0 +1,44 @@
+package de.ssherlock.control.util;
+
+import jakarta.faces.context.ExceptionHandler;
+import jakarta.faces.context.ExceptionHandlerFactory;
+import jakarta.faces.context.ExceptionHandlerWrapper;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.event.ExceptionQueuedEvent;
+
+import java.util.Iterator;
+
+public class ApplicationExceptionHandler extends ExceptionHandlerWrapper {
+
+    public ApplicationExceptionHandler(ExceptionHandler wrapped) {
+        super(wrapped);
+    }
+
+    @Override
+    public void handle() {
+        doHandle(FacesContext.getCurrentInstance());
+        getWrapped().handle();
+    }
+
+    public void doHandle(FacesContext context) {
+        Iterator<ExceptionQueuedEvent> exceptionQueuedEventIterator = getHandledExceptionQueuedEvents().iterator();
+
+        Throwable unhandledException = exceptionQueuedEventIterator.next().getContext().getException();
+
+        // deal with exceptions
+
+    }
+
+    public static class Factory extends ExceptionHandlerFactory {
+
+        public Factory(ExceptionHandlerFactory wrapped) {
+            super(wrapped);
+        }
+
+        @Override
+        public ExceptionHandler getExceptionHandler() {
+            return new ApplicationExceptionHandler(getWrapped().getExceptionHandler());
+        }
+    }
+
+}
