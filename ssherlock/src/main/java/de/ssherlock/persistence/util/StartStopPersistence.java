fc3f0804bcf53818014d94be8d02ab207e6ff6c6
Victor Vollmann <vollmannv@gmail.com>
2023-12-04 10:06:52 +0100

    Implement StartStop on all Layers

---
+++ ssherlock/src/main/java/de/ssherlock/persistence/util/StartStopPersistence.java
@@ -0,0 +1,72 @@
+package de.ssherlock.persistence.util;
+
+import de.ssherlock.global.logging.LoggerCreator;
+import de.ssherlock.global.logging.SerializableLogger;
+import de.ssherlock.persistence.config.Configuration;
+import de.ssherlock.persistence.connection.ConnectionPoolPsql;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletContextEvent;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * Class for managing initialization and destruction of the persistence layer during application startup and shutdown.
+ *
+ * @author Leon Höfling
+ */
+@ApplicationScoped
+public class StartStopPersistence implements Serializable {
+
+    /**
+     * Serial Version UID
+     */
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Configuration instance.
+     */
+    @Inject
+    private Configuration configuration;
+
+    /**
+     * Connection pool instance
+     */
+    @Inject
+    private ConnectionPoolPsql connectionPoolPsql;
+
+    /**
+     * Logger instance for logging messages related to the StartStopPersistence class.
+     */
+    @Inject
+    private SerializableLogger logger;
+
+    /**
+     * Default constructor.
+     */
+    public StartStopPersistence() {
+
+    }
+
+    /**
+     * Initializes the persistence layer during application startup.
+     *
+     * @param sce   The Servlet Context Event.
+     */
+    public void init(ServletContextEvent sce) {
+        LoggerCreator.readConfig(sce);
+        configuration.init(sce);
+        connectionPoolPsql.init();
+        logger.info("Persistence Layer initialized.");
+    }
+    /**
+     * Destroys the persistence layer during application shutdown.
+     */
+    public void destroy() {
+        connectionPoolPsql.destroy();
+        logger.info("Persistence Layer destroyed");
+    }
+
+}
