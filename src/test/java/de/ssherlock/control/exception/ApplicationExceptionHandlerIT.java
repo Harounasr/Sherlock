b75ee4ed251eb4da6016d7b9d9502e9ba2f14bd6
leonf <leonfoeck@gmail.com>
2023-12-07 15:49:25 +0100

    Added IT for ExceptionHandler and implemented the rest of the exception handler.

--- src/test/java/de/ssherlock/control/exception/ApplicationExceptionHandlerIT.java
+++ src/test/java/de/ssherlock/control/exception/ApplicationExceptionHandlerIT.java
@@ -1,17 +1,23 @@
 package de.ssherlock.control.exception;
 
+import jakarta.faces.component.UIViewRoot;
 import jakarta.faces.context.ExceptionHandler;
 import jakarta.faces.context.FacesContext;
+import jakarta.faces.context.PartialViewContext;
 import jakarta.faces.event.ExceptionQueuedEvent;
-import jakarta.faces.event.ExceptionQueuedEventContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,41 +25,41 @@ public class ApplicationExceptionHandlerIT {
 
     @Mock
     private ExceptionHandler wrappedExceptionHandler;
+
     @Mock
     private FacesContext facesContext;
+
     @Mock
-    private Iterator<ExceptionQueuedEvent> exceptionQueueIterator;
+    private PartialViewContext partialViewContext;
+
     @Mock
-    private ExceptionQueuedEventContext eventContext;
+    private UIViewRoot viewRoot;
 
     private ApplicationExceptionHandler exceptionHandler;
 
     @BeforeEach
     public void setUp() {
-        when(wrappedExceptionHandler.getUnhandledExceptionQueuedEvents()).thenReturn((Iterable<ExceptionQueuedEvent>) exceptionQueueIterator);
         exceptionHandler = new ApplicationExceptionHandler(wrappedExceptionHandler);
+        lenient().when(facesContext.getPartialViewContext()).thenReturn(partialViewContext);
+        lenient().when(facesContext.getViewRoot()).thenReturn(viewRoot);
     }
 
     @Test
-    public void testHandleNoAccessException() {
-        // Mocking exception and queue
-        NoAccessException noAccessException = new NoAccessException("No Access");
-        when(eventContext.getException()).thenReturn(noAccessException);
-        when(exceptionQueueIterator.hasNext()).thenReturn(true, false);
-        when(exceptionQueueIterator.next()).thenReturn(new ExceptionQueuedEvent(eventContext));
+    public void testHandleNoExceptions() {
+        // Prepare an empty exception queue
+        Queue<ExceptionQueuedEvent> events = new LinkedList<>();
+        when(wrappedExceptionHandler.getUnhandledExceptionQueuedEvents()).thenReturn(events);
 
         // Call the handle method
         exceptionHandler.handle();
 
-        // Verify the expected outcome, such as redirecting to the correct error page
-        // ... Add your verification logic here
+        // Verify that no further interactions occur
+        verify(wrappedExceptionHandler, times(1)).handle();
+        verifyNoMoreInteractions(facesContext);
     }
 
-    @Test
-    public void testHandleDBUnavailableException() {
-
-    }
-
-    // Additional tests for other scenarios and exceptions can be added here
-
+    // Additional tests for different exception types and scenarios,
+    // For example, test handling a DBUnavailableException, ConfigNotReadableException, etc.
 }
+
+
