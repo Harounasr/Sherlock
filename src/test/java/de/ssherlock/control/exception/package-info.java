61c6c950a54f63e5e608f2bc9223dacb01117ec1
leonf <leonfoeck@gmail.com>
2023-12-07 16:09:09 +0100

    Added missing package-info.java files.

---
+++ src/test/java/de/ssherlock/control/exception/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the tests for the ExceptionHandler.
+ */
+package de.ssherlock.control.exception;
---
+++ src/test/java/de/ssherlock/control/session/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the tests for the exception classes.
+ */
+package de.ssherlock.control.session;
