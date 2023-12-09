5875012897fa435b54051651b1785de7b7b6cb05
Leon Hoefling <leon@galerie-hoefling.de>
2023-12-09 18:30:01 +0100

    add tests for PasswordHashing.java

---
+++ src/test/java/de/ssherlock/business/util/PasswordHashingIT.java
@@ -0,0 +1,33 @@
+package de.ssherlock.business.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import de.ssherlock.global.transport.Password;
+import org.junit.jupiter.api.Test;
+
+public class PasswordHashingIT {
+
+  @Test
+  void testGetHashedPasswordWithRandomSalt() {
+    String password = "testPassword";
+    Password hashedPassword = PasswordHashing.getHashedPassword(password);
+
+    assertNotNull(hashedPassword);
+    assertNotNull(hashedPassword.getHash());
+    assertNotNull(hashedPassword.getSalt());
+    assertNotEquals(password, hashedPassword.getHash());
+  }
+
+  @Test
+  void testGetHashedPasswordWithGivenSalt() {
+    String password = "testPassword";
+    String salt = "randomSalt";
+
+    String hashedPassword = PasswordHashing.getHashedPassword(password, salt);
+
+    assertNotNull(hashedPassword);
+    assertNotEquals(password, hashedPassword);
+  }
+}
