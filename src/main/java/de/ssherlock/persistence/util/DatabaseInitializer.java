9c02d5e44adb748faea6349b0fc4051e4d47dc11
Leon Hoefling <leon@galerie-hoefling.de>
2023-12-09 11:21:57 +0100

    updated DatabaseInitializer.java

--- src/main/java/de/ssherlock/persistence/util/DatabaseInitializer.java
+++ src/main/java/de/ssherlock/persistence/util/DatabaseInitializer.java
@@ -2,16 +2,14 @@ package de.ssherlock.persistence.util;
 
 import de.ssherlock.global.logging.LoggerCreator;
 import de.ssherlock.global.logging.SerializableLogger;
-import jakarta.servlet.ServletContextEvent;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.function.Function;
+import java.util.logging.Level;
 
 /**
  * Offers database initialization functionalities.
@@ -20,15 +18,14 @@ import java.sql.Statement;
  */
 public final class DatabaseInitializer {
 
-    /**
-     * The logger of this class.
-     */
+    /** The Logger of this class. */
     private static final SerializableLogger LOGGER = LoggerCreator.get(DatabaseInitializer.class);
 
     /**
      * The path to the database initialization file.
      */
-    private static final String DATABASE_INITIALIZATION_PATH = "";
+    private static final String DATABASE_INITIALIZATION_PATH =
+            "WEB-INF/classes/de/ssherlock/sql/create_scheme.sql";
 
     /**
      * Default private empty constructor.
@@ -36,55 +33,32 @@ public final class DatabaseInitializer {
     private DatabaseInitializer() {}
 
     /**
-     * Initializes the database schema, if it has not already been initialized.
+     * Initializes the database.
      *
-     * @param sce The Servlet Context Event for initialization.
      * @param connection A connection to the database.
      */
-    public static void initialize(ServletContextEvent sce, Connection connection) {
-        String sql;
-        try (InputStream input = sce.getServletContext().getResourceAsStream(DATABASE_INITIALIZATION_PATH)) {
+    public static void initializeDatabase(
+            Function<String, InputStream> resourceFetcher, Connection connection) {
+        String sqlScript;
+        try (InputStream input = resourceFetcher.apply(DATABASE_INITIALIZATION_PATH)) {
             if (input == null) {
-                throw new FileNotFoundException("database initialization file could not be found at location: " + DATABASE_INITIALIZATION_PATH + ".");
+                throw new IOException("Could not find the file " + DATABASE_INITIALIZATION_PATH + ".");
             }
-            sql = new String(input.readAllBytes(), StandardCharsets.UTF_8);
+            sqlScript = new String(input.readAllBytes(), StandardCharsets.UTF_8);
 
-            LOGGER.fine("database initialization file loaded.");
+            LOGGER.fine("DB init loaded.");
         } catch (IOException e) {
-            throw new RuntimeException("The database initialization file could not be read", e);
+            LOGGER.log(Level.SEVERE, "DB init could not be loaded " + e.getMessage(), e);
+            throw new RuntimeException();
         }
         try {
-            if (!alreadyInitialized(connection)) {
-                try (Statement statement = connection.createStatement()) {
-                    statement.executeUpdate(sql);
-                    LOGGER.info("The database was successfully initialized.");
-                }
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(sqlScript);
+                LOGGER.info("Database initialized.");
             }
         } catch (SQLException e) {
-            throw new RuntimeException("The database could not be initialized", e);
+            LOGGER.log(Level.SEVERE, "Database could not be initialized " + e.getMessage(), e);
+            throw new RuntimeException();
         }
-    }
-
-    /**
-     * Checks whether the database exists.
-     *
-     * @param connection The connection to the database.
-     * @return Whether the database exists.
-     *
-     * @throws SQLException when the statement can not be executed.
-     */
-    private static boolean alreadyInitialized(Connection connection) throws SQLException {
-        boolean result = false;
-        try (Statement statement = connection.createStatement()) {
-            try (ResultSet set = statement.executeQuery("""
-                SELECT EXISTS (SELECT 1 FROM information_schemata.schemata
-                WHERE schema_name = 'ssherlock');
-                """)) {
-                if (set.next()) {
-                    result = set.getBoolean(1);
-                }
-            }
-        }
-        return result;
     }
 }
