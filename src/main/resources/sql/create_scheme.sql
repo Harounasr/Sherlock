f6c433007b92514c118872d08e4f156af94c075c
Leon Hoefling <leon@galerie-hoefling.de>
2023-12-09 11:20:59 +0100

    csv file updated

--- src/main/resources/sql/create_scheme.sql
+++ src/main/resources/sql/create_scheme.sql
@@ -1,14 +1,37 @@
-CREATE TYPE course_role AS ENUM ('TEACHER', 'TUTOR', 'MEMBER');
 
-CREATE TYPE system_role AS ENUM ('REGISTERED', 'TEACHER', 'ADMINISTRATOR');
+DO $$
+BEGIN
+        -- Check if course_role enum type exists
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'course_role') THEN
+            EXECUTE 'CREATE TYPE course_role AS ENUM (''TEACHER'', ''TUTOR'', ''MEMBER'')';
+END IF;
 
-CREATE TYPE grade AS ENUM ('1', '2', '3', '4', '5', '6');
+        -- Check if system_role enum type exists
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'system_role') THEN
+            EXECUTE 'CREATE TYPE system_role AS ENUM (''REGISTERED'', ''TEACHER'', ''ADMINISTRATOR'')';
+END IF;
 
-CREATE TYPE checker_type AS ENUM ('COMPILATION','IDENTITY','LINE_WIDTH','USER_DEFINED');
+        -- Check if grade enum type exists
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'grade') THEN
+            EXECUTE 'CREATE TYPE grade AS ENUM (''1'', ''2'', ''3'', ''4'', ''5'', ''6'')';
+END IF;
 
-CREATE TABLE user
+        -- Check if checker_type enum type exists
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'checker_type') THEN
+            EXECUTE 'CREATE TYPE checker_type AS ENUM (''COMPILATION'',''IDENTITY'',''LINE_WIDTH'',''USER_DEFINED'')';
+END IF;
+END $$;
+
+CREATE TABLE IF NOT EXISTS faculty
 (
-    username      VARCHAR(50) PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
+    name    VARCHAR(50) UNIQUE NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS "user"
+(
+    id            SERIAL PRIMARY KEY NOT NULL,
+    username      VARCHAR(50) UNIQUE      NOT NULL,
     email         VARCHAR(50) UNIQUE      NOT NULL,
     firstname     VARCHAR(50)             NOT NULL,
     lastname      VARCHAR(50)             NOT NULL,
@@ -17,55 +40,45 @@ CREATE TABLE user
     password_salt VARCHAR(50)             NOT NULL,
     user_role     system_role             NOT NULL,
     failed_login_attempts INTEGER         DEFAULT 0,
+    token         VARCHAR(75)             NOT NULL,
+    expiry_date   TIMESTAMP               WITH TIME ZONE NOT NULL,
 
-    FOREIGN KEY (faculty) REFERENCES faculty (name) ON DELETE SET DEFAULT
-);
+                                              FOREIGN KEY (faculty) REFERENCES faculty (name) ON DELETE SET DEFAULT
+    );
 
-CREATE TABLE not_verified_user
+CREATE TABLE IF NOT EXISTS course
 (
-    username  VARCHAR(50) PRIMARY KEY NOT NULL,
-    email     VARCHAR(50) UNIQUE      NOT NULL,
-    token     VARCHAR(75)             NOT NULL,
-    firstname VARCHAR(50)             NOT NULL,
-    lastname  VARCHAR(50)             NOT NULL,
-    faculty   VARCHAR(50)             DEFAULT 'NONE',
-    expiry_date TIMESTAMP             WITH TIME ZONE NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
+    course_name VARCHAR(50) UNIQUE NOT NULL
+    );
 
-    FOREIGN KEY (faculty) REFERENCES faculty (name) ON DELETE SET DEFAULT
-);
-
-CREATE TABLE course
-(
-    course_name VARCHAR(50) PRIMARY KEY NOT NULL,
-);
-
-CREATE TABLE exercise
+CREATE TABLE IF NOT EXISTS exercise
 (
     id                   SERIAL PRIMARY KEY NOT NULL,
-    name                 VARCHAR(200 UNIQUE NOT NULL,
+    name                 VARCHAR(200) UNIQUE NOT NULL,
     course_name          VARCHAR(50)        NOT NULL,
     publish_date         TIMESTAMP          WITH TIME ZONE NOT NULL,
     recommended_deadline TIMESTAMP          WITH TIME ZONE NOT NULL,
     obligatory_deadline  TIMESTAMP          WITH TIME ZONE NOT NULL,
-    description          text,
+                                                description          TEXT,
 
-    FOREIGN KEY (course_name) REFERENCES course (course_name) ON DELETE CASCADE,
+                                                FOREIGN KEY (course_name) REFERENCES course (course_name) ON DELETE CASCADE,
     CONSTRAINT date_constraints CHECK (
-                publish_date < recommended_deadline AND
-                recommended_deadline < obligatory_deadline
-        )
-);
+                                          publish_date < recommended_deadline AND
+                                          recommended_deadline < obligatory_deadline
+                                      )
+    );
 
-CREATE TABLE exercise_image
+CREATE TABLE IF NOT EXISTS exercise_image
 (
     uuid           uuid PRIMARY KEY NOT NULL,
     exercise_id    INTEGER          NOT NULL,
     exercise_image BYTEA,
 
     FOREIGN KEY (exercise_id) REFERENCES exercise (id) ON DELETE CASCADE
-);
+    );
 
-CREATE TABLE checker
+CREATE TABLE IF NOT EXISTS checker
 (
     id          SERIAL      NOT NULL,
     exercise_id INTEGER     NOT NULL,
@@ -77,19 +90,18 @@ CREATE TABLE checker
 
     PRIMARY KEY (id, exercise_id),
     FOREIGN KEY (exercise_id) REFERENCES exercise (id) ON DELETE CASCADE
-);
+    );
 
-CREATE TABLE submission
+CREATE TABLE IF NOT EXISTS submission
 (
     id                   SERIAL PRIMARY KEY NOT NULL,
-    timestamp_submission WITH TIME ZONE NOT NULL,
-    student_username     VARCHAR(50)        REFERENCES user(username) ON DELETE CASCADE,
-    tutor_username       VARCHAR(50)        REFERENCES user (username) ON DELETE SET NULL,
-    exercise_id          INTEGER REFERENCES exercise (id) ON DELETE CASCADE,
+    timestamp_submission TIMESTAMP WITH TIME ZONE NOT NULL,
+    student_username     VARCHAR(50)        REFERENCES "user"(username) ON DELETE CASCADE,
+    tutor_username       VARCHAR(50)        REFERENCES "user" (username) ON DELETE SET NULL,
+    exercise_id          INTEGER REFERENCES exercise (id) ON DELETE CASCADE
+    );
 
-);
-
-CREATE TABLE checker_result
+CREATE TABLE IF NOT EXISTS checker_result
 (
     exercise_id        INTEGER NOT NULL,
     checker_id         INTEGER NOT NULL,
@@ -100,33 +112,34 @@ CREATE TABLE checker_result
     PRIMARY key (Exercise_id, checker_id, submission_id),
     FOREIGN KEY (checker_id, Exercise_id) REFERENCES checker (id, exercise_id) ON DELETE CASCADE,
     FOREIGN KEY (submission_id) REFERENCES submission (id) ON DELETE CASCADE
-);
+    );
 
-CREATE TABLE submission_file
+CREATE TABLE IF NOT EXISTS submission_file
 (
-    id            SERIAL INTEGER NOT NULL,
+    id            SERIAL PRIMARY KEY NOT NULL,
     submission_id INTEGER NOT NULL,
     file_name     VARCHAR(50) NOT NULL,
     file          BYTEA,
 
-    PRIMARY KEY (id, submission_id)
-        FOREIGN KEY (submission_id) REFERENCES submission(id) ON DELETE CASCADE
-);
+    FOREIGN KEY (submission_id) REFERENCES submission(id) ON DELETE CASCADE
+    );
 
-CREATE TABLE testate_comment
+CREATE TABLE IF NOT EXISTS testate_comment
 (
     comment_id    SERIAL  NOT NULL,
+    submission_id INTEGER NOT NULL,
     file_id       INTEGER NOT NULL,
     is_visible    BOOLEAN NOT NULL,
     line_number   INTEGER NOT NULL,
     comment       TEXT    NOT NULL,
 
     PRIMARY KEY (submission_id, file_id, comment_id),
-    FOREIGN KEY (file_id) REFERENCES submission_file (submission_id, id) ON DELETE CASCADE,
-);
+    FOREIGN KEY (file_id) REFERENCES submission_file (id) ON DELETE CASCADE
+    );
 
-CREATE TABLE testate
+CREATE TABLE IF NOT EXISTS testate
 (
+    id SERIAL PRIMARY KEY NOT NULL,
     submission_id       INTEGER NOT NULL,
     tutor_id            INTEGER NOT NULL,
     layout_grade        grade   NOT NULL,
@@ -134,12 +147,11 @@ CREATE TABLE testate
     readability_grade   grade   NOT NULL,
     functionality_grade grade   NOT NULL,
 
-    PRIMARY key (submission_id),
     FOREIGN KEY (submission_id) REFERENCES submission (id) ON DELETE CASCADE,
-    FOREIGN KEY (tutor_id) REFERENCES user (username) ON DELETE SET NULL
-);
+    FOREIGN KEY (tutor_id) REFERENCES "user" (id) ON DELETE SET NULL
+    );
 
-CREATE TABLE system_settings
+CREATE TABLE IF NOT EXISTS system_settings
 (
     email_regex         VARCHAR(255),
     primary_color_hex   VARCHAR(6),
@@ -148,22 +160,17 @@ CREATE TABLE system_settings
     system_logo         BYTEA,
     imprint             TEXT,
     contact_information TEXT
-);
+    );
 
-CREATE TABLE faculty
+CREATE TABLE IF NOT EXISTS participates
 (
-    name    VARCHAR(50)   PRIMARY KEY NOT NULL,
-);
+    user_id    INTEGER     NOT NULL,
+    course_id  INTEGER     NOT NULL,
+    user_role  course_role NOT NULL,
 
-CREATE TABLE participates
-(
-    username    INTEGER     NOT NULL,
-    course_name VARCHAR     NOT NULL,
-    user_role   course_role NOT NULL,
-
-    PRIMARY KEY (username, course_name),
-    FOREIGN KEY (username) REFERENCES username ON DELETE CASCADE,
-    FOREIGN KEY (course_name) REFERENCES course (course_name) ON DELETE CASCADE
-);
+    PRIMARY KEY (user_id, course_id),
+    FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES course (id) ON DELETE CASCADE
+    );
 
 
