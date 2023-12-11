358d0ecf2f3d2cfb963e2c1069be5fbfed77d5d6
Leon Hoefling <leon@galerie-hoefling.de>
2023-12-11 16:15:35 +0100

    implemented password reset

--- src/main/java/de/ssherlock/control/backing/PasswordResetBean.java
+++ src/main/java/de/ssherlock/control/backing/PasswordResetBean.java
@@ -1,11 +1,15 @@
 package de.ssherlock.control.backing;
 
+import de.ssherlock.business.exception.BusinessNonExistentUserException;
 import de.ssherlock.business.service.UserService;
 import de.ssherlock.global.logging.SerializableLogger;
 import de.ssherlock.global.transport.User;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import java.util.Map;
+import java.util.logging.Level;
 
 /**
  * Backing bean for the passwordReset.xhtml facelet.
@@ -16,52 +20,106 @@ import jakarta.inject.Named;
 @RequestScoped
 public class PasswordResetBean {
 
-    /** The logger for logging events. */
-    private final SerializableLogger logger;
+  /** The logger for logging events. */
+  private final SerializableLogger logger;
 
-    /** The service for user-related operations. */
-    private final UserService userService;
+  /** The service for user-related operations. */
+  private final UserService userService;
 
-    /** The user for password reset. */
-    private User user;
+  /** The new password. */
+  private String passwordOne;
 
-    /**
-     * Constructor for PasswordForgottenBean.
-     *
-     * @param logger The logger for logging events (Injected).
-     * @param userService The service for user-related operations (Injected).
-     */
-    @Inject
-    public PasswordResetBean(SerializableLogger logger, UserService userService) {
-        this.logger = logger;
-        this.userService = userService;
-        user = new User();
+  /** The repeated password. */
+  private String passwordTwo;
+
+  /** The user for password reset. */
+  private User user;
+
+  /**
+   * Constructor for PasswordForgottenBean.
+   *
+   * @param logger The logger for logging events (Injected).
+   * @param userService The service for user-related operations (Injected).
+   */
+  @Inject
+  public PasswordResetBean(SerializableLogger logger, UserService userService) {
+    this.logger = logger;
+    this.userService = userService;
+    user = new User();
+  }
+
+  /**
+   * Navigates to the login page.
+   *
+   * @return The destination view for the login page.
+   */
+  public String navigateToLogin() {
+    return "/view/public/login.xhtml";
+  }
+
+  /** Reset the password. */
+  public void resetPassword() {
+    Map<String, String> parameter =
+        FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+    user.setVerificationToken(parameter.get("token"));
+    try {
+      userService.updateUser(user);
+    } catch (BusinessNonExistentUserException e) {
+      logger.log(Level.INFO, "No such user.");
     }
+  }
 
-    /**
-     * Navigates to the login page.
-     *
-     * @return The destination view for the login page.
-     */
-    public String navigateToLogin() {
-        return "/view/public/login.xhtml";
-    }
+  /**
+   * Gets the user.
+   *
+   * @return the user.
+   */
+  public User getUser() {
+    return user;
+  }
 
-    /**
-     * Gets the user.
-     *
-     * @return the user.
-     */
-    public User getUser() {
-        return user;
-    }
+  /**
+   * Sets the user password reset.
+   *
+   * @param user The user for the password reset.
+   */
+  public void setUser(User user) {
+    this.user = user;
+  }
 
-    /**
-     * Sets the user password reset.
-     *
-     * @param user The user for the password reset.
-     */
-    public void setUser(User user) {
-        this.user = user;
-    }
+  /**
+   * Gets the new password.
+   *
+   * @return The new password.
+   */
+  public String getPasswordOne() {
+    return passwordOne;
+  }
+
+  /**
+   * Sets the new password.
+   *
+   * @param passwordOne The new password.
+   */
+  public void setPasswordOne(String passwordOne) {
+    this.passwordOne = passwordOne;
+  }
+
+  /**
+   * Gets the repeated password.
+   *
+   * @return The repeated password.
+   */
+  public String getPasswordTwo() {
+    return passwordTwo;
+  }
+
+  /**
+   * Sets the repeated password.
+   *
+   * @param passwordTwo The repeated password.
+   */
+  public void setPasswordTwo(String passwordTwo) {
+    this.passwordTwo = passwordTwo;
+  }
 }
