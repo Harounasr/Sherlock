75844857c553671fc245b54dccaf7a6bd7f21158
Leon Hoefling <leon@galerie-hoefling.de>
2023-12-15 12:35:14 +0100

    added first implementation

--- src/main/java/de/ssherlock/global/transport/Testate.java
+++ src/main/java/de/ssherlock/global/transport/Testate.java
@@ -12,182 +12,251 @@ import java.util.Objects;
  */
 public class Testate implements Serializable {
 
-  /** Serial Version UID. */
-  @Serial private static final long serialVersionUID = 1L;
+    /**
+     * Serial Version UID.
+     */
+    @Serial
+    private static final long serialVersionUID = 1L;
 
-  /** The username of the evaluator. */
-  private String evaluator;
+    /**
+     * The username of the evaluator.
+     */
+    private long evaluatorId;
 
-  /** The username of the student. */
-  private String student;
+    /**
+     * The username of the student.
+     */
+    private String student;
 
-  /** The functionality grade. */
-  private int functionalityGrade;
+    /**
+     * The functionality grade.
+     */
+    private int functionalityGrade;
 
-  /** The readability grade. */
-  private int readabilityGrade;
+    /**
+     * The readability grade.
+     */
+    private int readabilityGrade;
 
-  /** List of TestateComments. */
-  private List<TestateComment> comments;
+    /**
+     * The layout grade.
+     */
+    private int layoutGrade;
 
-  /** Comment on the testate. */
-  private String comment;
+    /**
+     * The structure grade.
+     */
+    private int structureGrade;
 
-  /** The submission associated with the testate. */
-  private Submission submission;
+    /**
+     * List of TestateComments.
+     */
+    private List<TestateComment> comments;
 
-  /** Instantiates a new Testate. */
-  public Testate() {}
+    /**
+     * Comment on the testate.
+     */
+    private String comment;
 
-  /**
-   * Gets evaluator.
-   *
-   * @return the evaluator
-   */
-  public String getEvaluator() {
-    return evaluator;
-  }
+    /**
+     * The submission associated with the testate.
+     */
+    private Submission submission;
 
-  /**
-   * Sets evaluator.
-   *
-   * @param evaluator the evaluator
-   */
-  public void setEvaluator(String evaluator) {
-    this.evaluator = evaluator;
-  }
+    /**
+     * Instantiates a new Testate.
+     */
+    public Testate() {}
 
-  /**
-   * Gets student.
-   *
-   * @return the student
-   */
-  public String getStudent() {
-    return student;
-  }
-
-  /**
-   * Sets student.
-   *
-   * @param student the student
-   */
-  public void setStudent(String student) {
-    this.student = student;
-  }
-
-  /**
-   * Gets functionality grade.
-   *
-   * @return the functionality grade
-   */
-  public int getFunctionalityGrade() {
-    return functionalityGrade;
-  }
-
-  /**
-   * Sets functionality grade.
-   *
-   * @param functionalityGrade the functionality grade
-   */
-  public void setFunctionalityGrade(int functionalityGrade) {
-    this.functionalityGrade = functionalityGrade;
-  }
-
-  /**
-   * Gets readability grade.
-   *
-   * @return the readability grade
-   */
-  public int getReadabilityGrade() {
-    return readabilityGrade;
-  }
-
-  /**
-   * Sets readability grade.
-   *
-   * @param readabilityGrade the readability grade
-   */
-  public void setReadabilityGrade(int readabilityGrade) {
-    this.readabilityGrade = readabilityGrade;
-  }
-
-  /**
-   * Gets comments.
-   *
-   * @return the comments
-   */
-  public List<TestateComment> getComments() {
-    return comments;
-  }
-
-  /**
-   * Sets comments.
-   *
-   * @param comments the comments
-   */
-  public void setComments(List<TestateComment> comments) {
-    this.comments = comments;
-  }
-
-  /**
-   * Gets comment.
-   *
-   * @return the comment
-   */
-  public String getComment() {
-    return comment;
-  }
-
-  /**
-   * Sets comment.
-   *
-   * @param comment the comment
-   */
-  public void setComment(String comment) {
-    this.comment = comment;
-  }
-
-  /**
-   * Gets submission.
-   *
-   * @return the submission
-   */
-  public Submission getSubmission() {
-    return submission;
-  }
-
-  /**
-   * Sets submission.
-   *
-   * @param submission the submission
-   */
-  public void setSubmission(Submission submission) {
-    this.submission = submission;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    /**
+     * Gets evaluatorId.
+     *
+     * @return The evaluatorId.
+     */
+    public long getEvaluatorId() {
+        return evaluatorId;
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Testate testate = (Testate) o;
-    return functionalityGrade == testate.functionalityGrade
-        && readabilityGrade == testate.readabilityGrade
-        && Objects.equals(evaluator, testate.evaluator)
-        && Objects.equals(student, testate.student)
-        && Objects.equals(comments, testate.comments)
-        && Objects.equals(comment, testate.comment)
-        && Objects.equals(submission, testate.submission);
-  }
 
-  /** {@inheritDoc} */
-  @Override
-  public int hashCode() {
-    return Objects.hash(
-        evaluator, student, functionalityGrade, readabilityGrade, comments, comment, submission);
-  }
+    /**
+     * Sets evaluatorId.
+     *
+     * @param evaluatorId Id of the evaluator.
+     */
+    public void setEvaluatorId(long evaluatorId) {
+        this.evaluatorId = evaluatorId;
+    }
+
+    /**
+     * Gets student.
+     *
+     * @return the student
+     */
+    public String getStudent() {
+        return student;
+    }
+
+    /**
+     * Sets student.
+     *
+     * @param student the student
+     */
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    /**
+     * Gets functionality grade.
+     *
+     * @return the functionality grade
+     */
+    public int getFunctionalityGrade() {
+        return functionalityGrade;
+    }
+
+    /**
+     * Sets functionality grade.
+     *
+     * @param functionalityGrade the functionality grade
+     */
+    public void setFunctionalityGrade(int functionalityGrade) {
+        this.functionalityGrade = functionalityGrade;
+    }
+
+    /**
+     * Gets readability grade.
+     *
+     * @return the readability grade
+     */
+    public int getReadabilityGrade() {
+        return readabilityGrade;
+    }
+
+    /**
+     * Sets readability grade.
+     *
+     * @param readabilityGrade the readability grade
+     */
+    public void setReadabilityGrade(int readabilityGrade) {
+        this.readabilityGrade = readabilityGrade;
+    }
+
+    /**
+     * Gets comments.
+     *
+     * @return the comments
+     */
+    public List<TestateComment> getComments() {
+        return comments;
+    }
+
+    /**
+     * Sets comments.
+     *
+     * @param comments the comments
+     */
+    public void setComments(List<TestateComment> comments) {
+        this.comments = comments;
+    }
+
+    /**
+     * Gets comment.
+     *
+     * @return the comment
+     */
+    public String getComment() {
+        return comment;
+    }
+
+    /**
+     * Sets comment.
+     *
+     * @param comment the comment
+     */
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    /**
+     * Gets submission.
+     *
+     * @return the submission
+     */
+    public Submission getSubmission() {
+        return submission;
+    }
+
+    /**
+     * Sets submission.
+     *
+     * @param submission the submission
+     */
+    public void setSubmission(Submission submission) {
+        this.submission = submission;
+    }
+
+    /**
+     * Gets the layout grade.
+     *
+     * @return The layout grade,
+     */
+    public int getLayoutGrade() {
+        return layoutGrade;
+    }
+
+    /**
+     * Sets the layout grade.
+     *
+     * @param layoutGrade The layout grade to set.
+     */
+    public void setLayoutGrade(int layoutGrade) {
+        this.layoutGrade = layoutGrade;
+    }
+
+    /**
+     * Gets the structure grade.
+     *
+     * @return The structure grade.
+     */
+    public int getStructureGrade() {
+        return structureGrade;
+    }
+
+    /**
+     * Sets the structure grade.
+     *
+     * @param structureGrade The structure grade to set.
+     */
+    public void setStructureGrade(int structureGrade) {
+        this.structureGrade = structureGrade;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Testate testate = (Testate) o;
+        return functionalityGrade == testate.functionalityGrade
+               && readabilityGrade == testate.readabilityGrade
+               && Objects.equals(evaluatorId, testate.evaluatorId)
+               && Objects.equals(student, testate.student)
+               && Objects.equals(comments, testate.comments)
+               && Objects.equals(comment, testate.comment)
+               && Objects.equals(submission, testate.submission);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                evaluatorId, student, functionalityGrade, readabilityGrade, comments, comment, submission);
+    }
 }
