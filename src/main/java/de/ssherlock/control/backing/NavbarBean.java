3bb57907b17a892cbbaf916f8105655e75f4f41b
Lenny <hohls@fim.uni-passau.de>
2023-12-19 12:10:11 +0100

    h:message with working example in CoursePaginationBean, no css fade

--- src/main/java/de/ssherlock/control/backing/NavbarBean.java
+++ src/main/java/de/ssherlock/control/backing/NavbarBean.java
@@ -5,8 +5,6 @@ import de.ssherlock.control.session.AppSession;
 import de.ssherlock.global.logging.SerializableLogger;
 import de.ssherlock.global.transport.SystemSettings;
 import jakarta.annotation.PostConstruct;
-import jakarta.faces.application.FacesMessage;
-import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
