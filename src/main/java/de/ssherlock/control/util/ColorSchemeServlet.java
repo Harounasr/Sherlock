6114f2a8b3e0ce579e090b211d262c263f83e56f
Victor Vollmann <vollmannv@gmail.com>
2024-01-02 19:28:35 +0100

    added servlet to retrieve stored color scheme

---
+++ src/main/java/de/ssherlock/control/util/ColorSchemeServlet.java
@@ -0,0 +1,64 @@
+package de.ssherlock.control.util;
+
+import de.ssherlock.business.service.SystemService;
+import de.ssherlock.global.logging.SerializableLogger;
+import de.ssherlock.global.transport.SystemSettings;
+import jakarta.inject.Inject;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Web Servlet to serve the applications color scheme.
+ *
+ * @author Victor Vollmann
+ */
+@WebServlet("/colorScheme.css")
+public class ColorSchemeServlet extends HttpServlet {
+
+    /**
+     * The logger instance for this class.
+     */
+    @Inject
+    private SerializableLogger logger;
+
+    /**
+     * System Settings Service to get the color scheme.
+     */
+
+    @Inject
+    private SystemService systemService;
+
+    /**
+     * Default constructor.
+     */
+    public ColorSchemeServlet() {
+
+    }
+
+    /**
+     * Returns a css file that sets global variables for the color scheme.
+     *
+     * @param request  The HTTPServletRequest.
+     * @param response The HTTPServletResponse.
+     * @throws IOException when the writer is not accessible.
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        logger.info("Request for color scheme received.");
+        SystemSettings systemSettings = systemService.getSystemSettings();
+        response.setContentType("text/css");
+        PrintWriter out = response.getWriter();
+        out.println(":root {");
+        out.println("  --primary-color: #" + systemSettings.getPrimaryColorHex() + ";");
+        out.println("  --secondary-color: #" + systemSettings.getSecondaryColorHex() + ";");
+        out.println("}");
+        logger.info("Request for color scheme handled.");
+    }
+
+}
