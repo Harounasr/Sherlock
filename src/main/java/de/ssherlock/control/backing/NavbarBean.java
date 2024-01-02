dd888bf42675f637c4b5b4728c4f8d32ed24dc40
Victor Vollmann <vollmannv@gmail.com>
2024-01-02 19:29:14 +0100

    updated css for main template and navbar

--- src/main/java/de/ssherlock/control/backing/NavbarBean.java
+++ src/main/java/de/ssherlock/control/backing/NavbarBean.java
@@ -5,11 +5,14 @@ import de.ssherlock.control.session.AppSession;
 import de.ssherlock.global.logging.SerializableLogger;
 import de.ssherlock.global.transport.SystemSettings;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Base64;
 
 /**
  * Backing bean for the navbar.xhtml facelet.
@@ -20,152 +23,178 @@ import java.io.Serializable;
 @ViewScoped
 public class NavbarBean implements Serializable {
 
-  /** Serial Version UID. */
-  @Serial private static final long serialVersionUID = 1L;
+    /**
+     * Serial Version UID.
+     */
+    @Serial
+    private static final long serialVersionUID = 1L;
 
-  /** The logger for this class. */
-  private final SerializableLogger logger;
+    /**
+     * The logger for this class.
+     */
+    private final SerializableLogger logger;
 
-  /** The active session. */
-  private final AppSession appSession;
+    /**
+     * The active session.
+     */
+    private final AppSession appSession;
 
-  /** The system service for system-related operations. */
-  private final SystemService systemService;
+    /**
+     * The system service for system-related operations.
+     */
+    private final SystemService systemService;
     /**
      * Boolean which list to display.
      */
     private boolean seeAllCourses;
 
+    /**
+     * The current system settings.
+     */
+    private SystemSettings systemSettings;
 
-    /** The current system settings. */
-  private SystemSettings systemSettings;
+    /**
+     * The current facelet as a string.
+     */
+    private String currentFacelet;
 
+    /**
+     * Constructor for NavbarBean.
+     *
+     * @param logger        The logger for logging events (Injected).
+     * @param appSession    The active session (Injected).
+     * @param systemService The system service for system-related operations.
+     */
+    @Inject
+    public NavbarBean(SerializableLogger logger, AppSession appSession, SystemService systemService) {
+        this.logger = logger;
+        this.appSession = appSession;
+        this.systemService = systemService;
+    }
 
+    /**
+     * Initializes the NavbarBean.
+     */
+    @PostConstruct
+    public void initialize() {
+        String viewId = FacesContext.getCurrentInstance().getViewRoot().getViewId();
+        currentFacelet = viewId.substring(viewId.lastIndexOf('/') + 1).split("\\.", 2)[0];
+        logger.info(currentFacelet);
+    }
 
-  /**
-   * Constructor for NavbarBean.
-   *
-   * @param logger The logger for logging events (Injected).
-   * @param appSession The active session (Injected).
-   * @param systemService The system service for system-related operations.
-   */
-  @Inject
-  public NavbarBean(SerializableLogger logger, AppSession appSession, SystemService systemService) {
-    this.logger = logger;
-    this.appSession = appSession;
-    this.systemService = systemService;
-  }
+    /**
+     * Logs out the current user.
+     *
+     * @return String to Login Page
+     */
+    public String logout() {
+        return appSession.logout();
+    }
 
-  /** Initializes the NavbarBean. */
-  @PostConstruct
-  public void initialize() {}
+    /**
+     * Navigates to the page displaying all courses.
+     *
+     * @return The destination view for all courses.
+     */
+    public String navigateToAllCourses() {
+        seeAllCourses = true;
+        return "/view/registered/coursePagination.xhtml?all=true";
+    }
 
-  /** Logs out the current user.
-   * @return String to Login Page */
-  public String logout() {
-   return appSession.logout();
-  }
-
-  /**
-   * Navigates to the page displaying all courses.
-   *
-   * @return The destination view for all courses.
-   */
-  public String navigateToAllCourses() {
-    seeAllCourses = true;
-      return "/view/registered/coursePagination.xhtml?all=true";
-  }
-
-  /**
-   * Navigates to the page displaying user's courses.
-   *
-   * @return The destination view for user's courses.
-   */
-  public String navigateToMyCourses() {
-    seeAllCourses = false;
-    return "/view/registered/coursePagination.xhtml?all=false";
-  }
+    /**
+     * Navigates to the page displaying user's courses.
+     *
+     * @return The destination view for user's courses.
+     */
+    public String navigateToMyCourses() {
+        seeAllCourses = false;
+        return "/view/registered/coursePagination.xhtml?all=false";
+    }
 
     /**
      * navigation String to Checker List.
+     *
      * @return String.
      */
-  public String navigateToCheckers() {
-    return "/view/registered/checkerList.xhtml";
-  }
+    public String navigateToCheckers() {
+        return "/view/registered/checkerList.xhtml";
+    }
 
-  /**
-   * Navigates to the user's profile page.
-   *
-   * @return The destination view for user's profile.
-   */
-  public String navigateToProfile() {
-    return "/view/registered/profile.xhtml";
-  }
+    /**
+     * Navigates to the user's profile page.
+     *
+     * @return The destination view for user's profile.
+     */
+    public String navigateToProfile() {
+        return "/view/registered/profile.xhtml";
+    }
 
-  /**
-   * Navigates to the admin settings page.
-   *
-   * @return The destination view for admin settings.
-   */
-  public String navigateToAdminSettings() {
-    return "/view/admin/admin.xhtml";
-  }
+    /**
+     * Navigates to the admin settings page.
+     *
+     * @return The destination view for admin settings.
+     */
+    public String navigateToAdminSettings() {
+        return "/view/admin/admin.xhtml";
+    }
 
-  /**
-   * Navigates to the help page.
-   *
-   * @return The destination view for help.
-   */
-  public String navigateToHelp() {
-    return "/view/public/registration.xhtml";
-  }
+    /**
+     * Navigates to the help page.
+     *
+     * @return The destination view for help.
+     */
+    public String navigateToHelp() {
+        return "/view/public/registration.xhtml";
+    }
 
-  /**
-   * Gets system settings.
-   *
-   * @return the system settings
-   */
-  public SystemSettings getSystemSettings() {
-    return systemSettings;
-  }
+    /**
+     * Gets system settings.
+     *
+     * @return the system settings
+     */
+    public SystemSettings getSystemSettings() {
+        return systemSettings;
+    }
 
-  /**
-   * Sets system settings.
-   *
-   * @param systemSettings the system settings
-   */
-  public void setSystemSettings(SystemSettings systemSettings) {
-    this.systemSettings = systemSettings;
-  }
+    /**
+     * Sets system settings.
+     *
+     * @param systemSettings the system settings
+     */
+    public void setSystemSettings(SystemSettings systemSettings) {
+        this.systemSettings = systemSettings;
+    }
 
-  /**
-   * Getter for the appSession.
-   *
-   * @return AppSession
-   */
-  public AppSession getAppSession() {
-    return appSession;
-  }
+    /**
+     * Getter for the appSession.
+     *
+     * @return AppSession
+     */
+    public AppSession getAppSession() {
+        return appSession;
+    }
 
-  /**
-   * Getter for the logo, stored in the database.
-   *
-   * @return byteArray for the Logo.
-   */
-  public byte[] getLogo() {
-    return systemService.getSystemSettings().getLogo();
-  }
+    /**
+     * Getter for the logo, stored in the database.
+     *
+     * @return byteArray for the Logo.
+     */
+    public String getLogoBase64() {
+        return Base64.getEncoder().encodeToString(systemService.getSystemSettings().getLogo());
+    }
 
     /**
      * Getter for the boolean.
+     *
      * @return boolean
      */
     public boolean isSeeAllCourses() {
         return seeAllCourses;
     }
+
     /**
      * Setter for the boolean.
+     *
      * @param seeAllCourses boolean
      */
 
@@ -173,4 +202,12 @@ public class NavbarBean implements Serializable {
         this.seeAllCourses = seeAllCourses;
     }
 
+    /**
+     * Gets current facelet.
+     *
+     * @return the current facelet
+     */
+    public String getCurrentFacelet() {
+        return currentFacelet;
+    }
 }
--- src/main/java/de/ssherlock/control/util/ExerciseDescriptionImageServlet.java
+++ src/main/java/de/ssherlock/control/util/ExerciseDescriptionImageServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.util.logging.Level;
 
@@ -20,68 +21,72 @@ import java.util.logging.Level;
 @WebServlet("/image")
 public class ExerciseDescriptionImageServlet extends HttpServlet {
 
-  /** The logger instance for this class. */
-  @Inject
-  private SerializableLogger logger;
+    /**
+     * The logger instance for this class.
+     */
+    @Inject
+    private SerializableLogger logger;
 
-  /** Exercise description image service for image related operations. */
-  @Inject
-  private ExerciseDescriptionImageService exerciseDescriptionImageService;
+    /**
+     * Exercise description image service for image related operations.
+     */
+    @Inject
+    private ExerciseDescriptionImageService exerciseDescriptionImageService;
 
-  /**
-   * Default constructor.
-   */
-  public ExerciseDescriptionImageServlet() {
+    /**
+     * Default constructor.
+     */
+    public ExerciseDescriptionImageServlet() {
 
-  }
-
-  /**
-   * Gets the requested image from the database and serves it through the response.
-   *
-   * @param request The HTTPServletRequest.
-   * @param response The HTTPServletResponse.
-   * @throws IOException when the writer is not accessible.
-   */
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
-    String imageId = request.getParameter("id");
-    if (imageId != null) {
-      logger.info("Client request for image with id " + imageId);
-      ExerciseDescriptionImage image = new ExerciseDescriptionImage();
-      image.setUUID(imageId);
-      try {
-        image = exerciseDescriptionImageService.getImage(image);
-        response.setContentType("image/png");
-        response.getOutputStream().write(image.getImage());
-        logger.info("Request managed successfully.");
-      } catch (IOException | BusinessNonExistentImageException e) {
-        handleException(response, e);
-      }
-    } else {
-      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      response.getWriter().println("Image ID parameter is missing");
-      logger.warning("Requested an image without use of id.");
     }
-  }
 
-  /**
-   * Handles exceptions that might occur during the image request handling process.
-   *
-   * @param response The HttpServletResponse object for sending the error response back to the
-   *     client.
-   * @param e The exception that occurred during the image request handling.
-   * @throws IOException If an I/O error occurs while sending the error response to the client.
-   */
-  private void handleException(HttpServletResponse response, Exception e) throws IOException {
-    if (e instanceof BusinessNonExistentImageException) {
-      logger.log(Level.WARNING, "Requested image does not exist.");
-      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-      response.getWriter().println("Image not found");
-    } else {
-      logger.log(Level.SEVERE, "Error while processing image request", e);
-      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-      response.getWriter().println("Error processing the request");
+    /**
+     * Gets the requested image from the database and serves it through the response.
+     *
+     * @param request  The HTTPServletRequest.
+     * @param response The HTTPServletResponse.
+     * @throws IOException when the writer is not accessible.
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        String imageId = request.getParameter("id");
+        if (imageId != null) {
+            logger.info("Client request for image with id " + imageId);
+            ExerciseDescriptionImage image = new ExerciseDescriptionImage();
+            image.setUUID(imageId);
+            try {
+                image = exerciseDescriptionImageService.getImage(image);
+                response.setContentType("image/png");
+                response.getOutputStream().write(image.getImage());
+                logger.info("Request managed successfully.");
+            } catch (IOException | BusinessNonExistentImageException e) {
+                handleException(response, e);
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().println("Image ID parameter is missing");
+            logger.warning("Requested an image without use of id.");
+        }
     }
-  }
+
+    /**
+     * Handles exceptions that might occur during the image request handling process.
+     *
+     * @param response The HttpServletResponse object for sending the error response back to the
+     *                 client.
+     * @param e        The exception that occurred during the image request handling.
+     * @throws IOException If an I/O error occurs while sending the error response to the client.
+     */
+    private void handleException(HttpServletResponse response, Exception e) throws IOException {
+        if (e instanceof BusinessNonExistentImageException) {
+            logger.log(Level.WARNING, "Requested image does not exist.");
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            response.getWriter().println("Image not found");
+        } else {
+            logger.log(Level.SEVERE, "Error while processing image request", e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("Error processing the request");
+        }
+    }
 }
