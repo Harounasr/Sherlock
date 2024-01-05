a124ca3a810c2fe2e4e781c8cdcf2d00ad1acd70
Victor Vollmann <vollmannv@gmail.com>
2024-01-05 14:44:58 +0100

    refactoring linter

--- src/main/webapp/resources/js/notifications.js
+++ src/main/webapp/resources/js/notifications.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const messages = document.querySelector('.popup-notifications')
   if (messages && messages.innerHTML.trim() !== '') {
-    messages.style.display = 'block';
+    messages.style.display = 'block'
     setTimeout(function () {
       messages.style.display = 'none'
     }, 5000)
