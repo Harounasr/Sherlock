54dd0db7f74528160f51149bf64c574a5a7109ad
Leon Hoefling <leon@galerie-hoefling.de>
2024-01-07 18:16:38 +0100

    Fixed linter errors.

--- src/main/java/de/ssherlock/business/service/UserService.java
+++ src/main/java/de/ssherlock/business/service/UserService.java
@@ -35,7 +35,6 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
