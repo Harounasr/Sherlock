c6bbc44d067bb82e4f7be06dee535c1c7e29927e
HarounASR <harounalswedany@gmail.com>
2024-01-10 13:30:33 +0100

    TestatePagination AllTestateBean submit

---
+++ target/classes/resources/js/notifications.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const messages = document.querySelector('.popup-notifications')
+  if (messages && messages.innerHTML.trim() !== '') {
+    messages.style.display = 'block'
+    setTimeout(function () {
+      messages.style.display = 'none'
+    }, 5000)
+  }
+})
