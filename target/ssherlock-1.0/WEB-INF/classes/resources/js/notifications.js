2a5d29d47735c554cc5a74768d07ba987e735481
HarounASR <harounalswedany@gmail.com>
2024-01-11 23:38:08 +0100

    "System settings"

--- target/ssherlock-1.0/WEB-INF/classes/resources/js/notifications.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const messages = document.querySelector('.popup-notifications')
-  if (messages && messages.innerHTML.trim() !== '') {
-    messages.style.display = 'block'
-    setTimeout(function () {
-      messages.style.display = 'none'
-    }, 5000)
-  }
-})
