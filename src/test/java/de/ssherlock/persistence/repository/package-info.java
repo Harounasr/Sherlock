99231f383782b892121b09c1f7ea57144c98fb7b
Victor Vollmann <vollmannv@gmail.com>
2024-01-14 11:46:51 +0100

    added test for TransactionPsql and added a LoadTest

--- src/test/java/de/ssherlock/persistence/repository/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Test classes for the persistence repository package.
- */
-package de.ssherlock.persistence.repository;
---
+++ src/test/java/de/ssherlock/persistence/transaction/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Test classes for the transaction package.
+ */
+package de.ssherlock.persistence.transaction;
