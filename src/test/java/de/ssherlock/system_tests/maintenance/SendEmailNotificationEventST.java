b3c911489a8780b21c1118858cf20c0b5a779ea7
Victor Vollmann <vollmannv@gmail.com>
2024-01-14 17:18:22 +0100

    added test for SendEmailNotificationEvent

--- src/test/java/de/ssherlock/system_tests/maintenance/SendEmailNotificationEventST.java
+++ src/test/java/de/ssherlock/system_tests/maintenance/SendEmailNotificationEventST.java
@@ -1,7 +1,11 @@
 package de.ssherlock.system_tests.maintenance;
 
 import de.ssherlock.system_tests.ui.SeleniumUITestUtils;
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,6 +24,8 @@ import static org.junit.jupiter.api.Assertions.fail;
  *
  * @author Victor Vollmann
  */
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+@Order(1)
 public class SendEmailNotificationEventST {
 
     /**
