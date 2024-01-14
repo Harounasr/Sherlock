e9c899da51194456f61e052497d5d03a908aa7bf
Leon Hoefling <leon@galerie-hoefling.de>
2024-01-14 11:02:16 +0100

    fix validator errors

--- src/test/java/de/ssherlock/system_tests/ui/SeleniumUITestUtils.java
+++ src/test/java/de/ssherlock/system_tests/ui/SeleniumUITestUtils.java
@@ -131,7 +131,7 @@ public final class SeleniumUITestUtils {
      * @param facesMessage The expected faces message.
      */
     public static void checkFacesMessage(WebDriverWait wait, FacesMessage facesMessage) {
-        WebElement element = wait.until(visibilityOfElementLocated(By.cssSelector(".popup-notifications .notification-success > td")));
+        WebElement element = wait.until(visibilityOfElementLocated(By.cssSelector(".popup-notifications .notification-success")));
         assertTrue(element.isDisplayed());
         assertTrue(element.getText().contains(facesMessage.getDetail()));
     }
