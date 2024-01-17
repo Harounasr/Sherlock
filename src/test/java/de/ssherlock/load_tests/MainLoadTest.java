fff190e502efdb69b2e2345071f259907bf58cef
Victor Vollmann <vollmannv@gmail.com>
2024-01-17 17:23:38 +0100

    added load test

--- src/test/java/de/ssherlock/load_tests/MainLoadTest.java
+++ src/test/java/de/ssherlock/load_tests/MainLoadTest.java
@@ -6,17 +6,26 @@ import de.ssherlock.system_tests.ui.facelets.LoginUITest;
 import org.jsmart.zerocode.core.domain.LoadWith;
 import org.jsmart.zerocode.core.domain.TestMapping;
 import org.jsmart.zerocode.core.domain.TestMappings;
-import org.jsmart.zerocode.core.runner.parallel.ZeroCodeMultiLoadRunner;
-import org.junit.runner.RunWith;
+import org.jsmart.zerocode.jupiter.extension.ParallelLoadExtension;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-@LoadWith("/load_tests/load-config.properties")
-@RunWith(ZeroCodeMultiLoadRunner.class)
-@TestMappings({
-        @TestMapping(testClass = LoginUITest.class, testMethod = "testLoginSuccess"),
-        @TestMapping(testClass = ExerciseDescriptionUITest.class, testMethod = "testEditDates"),
-        @TestMapping(testClass = ExerciseDescriptionUITest.class, testMethod = "testEditHTMLSuccess"),
-        @TestMapping(testClass = ExerciseDescriptionUITest.class, testMethod = "testEditHTMLFailure"),
-        @TestMapping(testClass = AdminUserPaginationUITest.class, testMethod = "testCheckSearch"),
-        @TestMapping(testClass = AdminUserPaginationUITest.class, testMethod = "testChangeRoles")
-})
-public class MainLoadTest {}
+@LoadWith("load_tests/load-config.properties")
+@ExtendWith(ParallelLoadExtension.class)
+public class MainLoadTest {
+
+    @Test
+    @DisplayName("Main Load Test")
+    @TestMappings({
+            @TestMapping(testClass = LoginUITest.class, testMethod = "testLoginSuccess"),
+            @TestMapping(testClass = ExerciseDescriptionUITest.class, testMethod = "testEditDates"),
+            @TestMapping(testClass = ExerciseDescriptionUITest.class, testMethod = "testEditHTMLSuccess"),
+            @TestMapping(testClass = ExerciseDescriptionUITest.class, testMethod = "testEditHTMLFailure"),
+            @TestMapping(testClass = AdminUserPaginationUITest.class, testMethod = "testCheckSearch"),
+            @TestMapping(testClass = AdminUserPaginationUITest.class, testMethod = "testChangeRoles")
+    })
+    public void load() {
+
+    }
+}
